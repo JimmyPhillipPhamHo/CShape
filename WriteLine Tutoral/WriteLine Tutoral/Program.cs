3a27a07256577681c878c5b96a0df96054215a93
JimmyPhillipPhamHo <jimmyphamho@yahoo.com>
2018-01-16 15:02:47 -0600

    WriteLine Tutoral

---
+++ WriteLine Tutoral/WriteLine Tutoral/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Hello World");
+
+            Console.WriteLine("Line one");
+            Console.WriteLine("Line two");
+            Console.WriteLine("Line three");
+
+            Console.WriteLine("XXXXXXXXXXXXX");
+            Console.WriteLine(" X         X");
+            Console.WriteLine("  X       X");
+            Console.WriteLine("   XXXXXXX");
+            Console.WriteLine("      X");
+            Console.WriteLine("      X");
+            Console.WriteLine("    XXXXX");
+
+            Console.WriteLine("H      H");
+            Console.WriteLine("H      H");
+            Console.WriteLine("H      H");
+            Console.WriteLine("HHHHHHHH");
+            Console.WriteLine("H      H");
+            Console.WriteLine("H      H");
+            Console.WriteLine("H      H");
+
+        }
+    }
+}
